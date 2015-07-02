0f2a32cd5c0ad8791c0a7f1739e11ae6aa992311
Cgeist7 <cgeist7@gmail.com>
2015-07-02 09:00:06 -0400

    test graph

--- challenge/models.py
+++ challenge/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Application(models.Model):
+    author = models.ForeignKey('auth.User')
+    name = models.CharField(max_length=128)
+    short_text = models.TextField()
+    detail_text = models.TextField()
+    submitted_date = models.DateTimeField(default=timezone.now)
+    published = models.BooleanField(default=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
